4887ee91e8a6a5df0ccd989bf73d7137c4c8d987
eugeniarossi <eugenia.rossi@outlook.com>
2023-02-22 20:45:56 +0100

    comportamento reset button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // Definisco la variabile button
-const button = document.getElementById('form_button');
+const genButton = document.getElementById('gen_button');
+const resetButton = document.getElementById('reset_button');
 
 // Definisco la variabile con il prezzo al km
 const priceKm = 0.21;
@@ -14,32 +15,40 @@ let inputKm;
 // Definisco la variabile price per il prezzo
 let price;
 
+const sectionTicket = document.getElementById('ticket-section');
+
 // Evento click del button
-button.addEventListener('click', 
+genButton.addEventListener('click', 
     function() {
 
         // Salvo i valori degli input nelle variabili
-        inputAge = Number(document.getElementById("input_age").value);
+        inputAge = document.getElementById("input_age").value;
         inputKm = Number(document.getElementById("input_km").value);
         inputName = document.getElementById('input_name').value;
         price = priceKm * inputKm;
 
         // Calcolo lo sconto e lo applico alla variabile price
-        if(inputAge < 18) {
+        if(inputAge === 'minorenne') {
             
             price = price * 0.8;
             
-        } else if (inputAge > 65) {
+        } else if (inputAge === 'over-65') {
 
             price = price * 0.6;
-            
         } 
 
         // Stampo il prezzo finale in forma umana in console
         console.log(`€ ${price.toFixed(2)}`);
 
         // Stampo nome passeggero e prezzo del biglietto sulla pagina
-        document.getElementById('nome_utente').innerHTML = inputName;
-        document.getElementById('prezzo_biglietto').innerHTML = price.toFixed(2);
+        document.getElementById('passenger_name').innerHTML = inputName;
+        document.getElementById('price').innerHTML = price.toFixed(2);
+
+        // Aggiungo (toggle) una classe per fa apparire la section biglietto
+        sectionTicket.classList.toggle('show');
 });
 
+resetButton.addEventListener('click', 
+    function () {
+        sectionTicket.classList.remove('show');
+})
